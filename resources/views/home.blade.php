71cd03a1ac5b235bf28cd5eaf454fb24d38231b7
Luis Leal <luisrrleal@gmail.com>
2025-02-03 20:58:08 -0600

    Start adding UI

---
+++ resources/views/home.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-3">
+            <img
+                src="https://instagram.fblr1-4.fna.fbcdn.net/v/t51.2885-19/s150x150/118752013_331688131171366_2926820730730732927_n.jpg?_nc_ht" />
+        </div>
+        <div class="col-9">
+            <div>
+                <h1>lealGram</h1>
+            </div>
+            <div class="d-flex">
+                <div class="pr-5"><strong>153</strong> posts</div>
+                <div class="pr-5"><strong>23k</strong> followers</div>
+                <div class="pr-5"><strong>212</strong> following</div>
+            </div>
+            <div class="pt-4 font-weight-bold">lealGram</div>
+            <div>lealGram</div>
+            <div><a href="#">lealGram.com</a></div>
+        </div>
+    </div>
+    @endsection
